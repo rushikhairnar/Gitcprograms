d121afa991181c94b6804ab5ae0c76849947505a
rushikhairnar <109852515+rushikhairnar@users.noreply.github.com>
2022-07-23 15:56:39 +0530

    First Commit

---
+++ project/add.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+	int a,b,c;
+	printf("\n enter a= ");
+	scanf("%d",&a);
+	printf("\n enter b= ");
+	scanf("%d",&b);
+	
+	c=a+b;
+	printf("\n enter add of two numbers=%d",c);
+}
